84c6e1959494e28d76562732f9843a513c1659bc
Pratik Dash <dashpratik40@gmail.com>
2023-01-01 00:45:07 +0530

    Finished setting up win conditions

--- app.js
+++ app.js
@@ -1,29 +1,96 @@
 //setting up gameboard using Module
-let gameBoard = (()=>{
+let winConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [3,5,9],
+    [0,4,8],
+    [2,4,6]
+]
 
-    let gameBoard = []
-    return {}
+let gameBoardModule = (()=>{
+
+    let gameBoard = ["","","","","","","","",""]
+    return {gameBoard}
 })();
 
-let displayController = (()=>{
+let gameModule = (()=>{
 
-    let diplayControllerTest = ()=>console.log("Testing display controller");
-    return{diplayControllerTest}
+    let currentPlayer = "X";
+    let statusText = document.querySelector(".status-text");
 
-})();
+    let gridBoxClickHandler = ()=>{
+        setStatus();
+        let gridBoxes = document.querySelectorAll(".cell");
+        gridBoxes.forEach(gridBox => gridBox.addEventListener('click',()=>{
 
-let player = (playerName, playerSymbol)=>{
+            let gridBoxIndex = gridBox.getAttribute("index")
+            if(gameBoardModule.gameBoard[gridBoxIndex] != ""){
+                return
+            }
+            updateGridBox(gridBox,gridBoxIndex);
+        }))
 
-    let getPlayerName  = ()=>console.log("Player Name: "+playerName)
-    let getPlayerSymbol = ()=>console.log("Player Symbol: "+playerSymbol)
-    return{
-        getPlayerName,
-        getPlayerSymbol
     }
+    let updateGridBox = (gridBox,index)=>{
 
-}
-displayController.diplayControllerTest()
-let Mark = player("Mark","X")
-let Jerry = player("Jerry","O")
-Mark.getPlayerName();
-Mark.getPlayerSymbol();
+        gameBoardModule.gameBoard[index] = currentPlayer
+        gridBox.textContent = currentPlayer
+        switchPlayer()
+        console.log(gameBoardModule.gameBoard)
+
+
+    }
+    let switchPlayer = ()=>{
+        currentPlayer === "X" ? currentPlayer = "O":currentPlayer = "X"
+        setStatus()
+    }
+    let setStatus = ()=>{
+        statusText.textContent = `Player ${currentPlayer}'s turn`
+    }
+    return{gridBoxClickHandler}
+})();
+
+gameModule.gridBoxClickHandler();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let displayControllerModule = (()=>{
+
+//    let renderGridBoxes = ()=>{
+//     let gridBoxes = document.querySelectorAll(".cell");
+//     gridBoxes.forEach(gridBox => gridBox.addEventListener("click",()=>{
+
+        
+//     }))}
+//    return{renderGridBoxes}
+
+// })()
+// let playerFactory = (playerName, playerSymbol)=>{
+
+//     let getPlayerName = ()=> playerName
+//     let getPlayerSymbol = ()=>playerSymbol
+//     return(getPlayerName,getPlayerSymbol)
+// }
+
+// let Optimus = playerFactory("Optimus","X");
+// let Megatron = playerFactory("Megatron","O");
+
+// displayControllerModule.renderGridBoxes();
